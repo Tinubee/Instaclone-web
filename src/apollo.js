4e65c95ba6c79ac4540db2b7bb958125268f08e9
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-05-31 18:15:13 +0900

    Production API URL

--- src/apollo.js
+++ src/apollo.js
@@ -31,7 +31,10 @@ export const disableDarkMode = () => {
 };
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://tinubee-instaclone-backend.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
